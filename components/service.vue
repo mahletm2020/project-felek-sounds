bc2b11174f223094d638be4c7a63df5a6d944372
Mahlet Mulugeta <161637299+mahletm2020@users.noreply.github.com>
2024-11-03 04:00:43 -0800

    service initial

---
+++ components/service.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    
+  </div>
+</template>
+
+<style scoped>
+body {
+  margin: 0;
+  padding: 0;
+}
+
+.home {
+  background-image: url('/images/dude2.jpg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
